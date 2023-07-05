ad54a3119a8f027411fe4820595926c5432add93
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-05 17:46:25 +0300

    Rename AuthResponse.java to JwtResponse.java for clarity

--- src/main/java/com/uptalent/auth/model/response/JwtResponse.java
+++ src/main/java/com/uptalent/auth/model/response/JwtResponse.java
@@ -1,4 +1,4 @@
-package com.uptalent.auth.model;
+package com.uptalent.auth.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthResponse {
+public class JwtResponse {
     private String jwt;
 }
