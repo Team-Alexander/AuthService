71dac5c13cebf968eb1917ef280d04506a1677ee
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-13 16:41:47 +0300

    Add ValidationExceptionHandler to handle validation exceptions

---
+++ src/main/java/com/uptalent/auth/exception/ValidationExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.uptalent.auth.exception;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        return fieldErrors.stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        fieldError -> Optional.ofNullable(fieldError.getDefaultMessage()).orElse("N/A")
+                ));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleConstraintViolationException(ConstraintViolationException ex) {
+        Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
+        return constraintViolations.stream()
+                .collect(Collectors.toMap(
+                        violation -> getFieldNameFromPath(violation.getPropertyPath()),
+                        ConstraintViolation::getMessage
+                ));
+    }
+
+    private String getFieldNameFromPath(Path propertyPath) {
+        String fullPath = propertyPath.toString();
+        int dotIndex = fullPath.lastIndexOf(".");
+        if (dotIndex != -1) {
+            return fullPath.substring(dotIndex + 1);
+        }
+        return fullPath;
+    }
+}
