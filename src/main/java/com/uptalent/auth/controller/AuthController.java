dcf1d611e7b0208e1fb70885835c6def8f8e05d7
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-13 16:41:30 +0300

    Add HTTP response status to register method in AuthController

--- src/main/java/com/uptalent/auth/controller/AuthController.java
+++ src/main/java/com/uptalent/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.uptalent.auth.model.PublicKeyDTO;
 import com.uptalent.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,6 +18,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
     public JwtResponse register(@Valid @RequestBody AuthRegister authRegister) {
         return authService.registerUser(authRegister);
     }
