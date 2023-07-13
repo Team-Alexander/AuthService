8c38023388d9ace5969507aada471f084776f6d8
Mark <42696773+MarkHmnv@users.noreply.github.com>
2023-07-13 16:56:55 +0300

    Update auth models (#8)
    * Update request models for authentication
    
    * Add HTTP response status to register method in AuthController
    
    * Add ValidationExceptionHandler to handle validation exceptions

--- src/main/java/com/uptalent/auth/model/request/AuthLogin.java
+++ src/main/java/com/uptalent/auth/model/request/AuthLogin.java
@@ -1,25 +1,14 @@
 package com.uptalent.auth.model.request;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        property = "type")
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = TalentRegister.class, name = "talentRegister"),
-        @JsonSubTypes.Type(value = SponsorRegister.class, name = "sponsorRegister")
-})
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
-public abstract class AuthRegister {
+public class AuthLogin {
     @Email
     @NotBlank(message = "Email should not be blank")
     @Size(max = 100, message = "Email must be less than 100 characters")
