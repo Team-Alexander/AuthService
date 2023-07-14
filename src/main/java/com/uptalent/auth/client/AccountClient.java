31dcf5eb854085a907b7bbfb2d7aae61f008face
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-07-14 14:49:06 +0300

    Create client for sending login request

--- src/main/java/com/uptalent/auth/client/AccountClient.java
+++ src/main/java/com/uptalent/auth/client/AccountClient.java
@@ -1,5 +1,6 @@
 package com.uptalent.auth.client;
 
+import com.uptalent.auth.model.request.AuthLogin;
 import com.uptalent.auth.model.request.AuthRegister;
 import com.uptalent.auth.model.response.AuthResponse;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -12,4 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface AccountClient {
     @PostMapping("/api/v1/account/save")
     AuthResponse save(@RequestBody AuthRegister authRegister);
+
+    @PostMapping("/api/v1/account/login")
+    AuthResponse login(@RequestBody AuthLogin authLogin);
 }
