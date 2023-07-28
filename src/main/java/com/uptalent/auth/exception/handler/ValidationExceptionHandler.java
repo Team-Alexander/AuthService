7e3200fb4c6af1139a4f2e3e2beb57e278922cfc
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-07-28 21:07:32 +0300

    Create and handle exception

--- src/main/java/com/uptalent/auth/exception/handler/ValidationExceptionHandler.java
+++ src/main/java/com/uptalent/auth/exception/handler/ValidationExceptionHandler.java
@@ -1,9 +1,11 @@
-package com.uptalent.auth.exception;
+package com.uptalent.auth.exception.handler;
 
+import com.uptalent.auth.exception.AccountNotFoundException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Path;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +17,12 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ValidationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(AccountNotFoundException.class)
+    public Map<String, Object> handlerNotFoundException(AccountNotFoundException e) {
+        return Map.of("message", e.getMessage());
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -36,6 +44,12 @@ public class ValidationExceptionHandler {
                         violation -> getFieldNameFromPath(violation.getPropertyPath()),
                         ConstraintViolation::getMessage
                 ));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Map<String, Object> handleBadCredentialsException(BadCredentialsException e) {
+        return Map.of("message", e.getMessage());
     }
 
     private String getFieldNameFromPath(Path propertyPath) {
