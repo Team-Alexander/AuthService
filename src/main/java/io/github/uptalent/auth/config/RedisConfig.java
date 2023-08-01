2e3507f2334fc70fc616dc8ae17dd0a033199339
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-08-01 17:02:33 +0300

    Add Redis configuration

---
+++ src/main/java/io/github/uptalent/auth/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package io.github.uptalent.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        final RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
