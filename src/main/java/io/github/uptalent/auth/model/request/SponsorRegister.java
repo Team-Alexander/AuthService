0d864344dcefc01f5ab137e4b40b532948e914b2
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-08-04 15:12:57 +0300

    fix: register models

--- src/main/java/io/github/uptalent/auth/model/request/SponsorRegister.java
+++ src/main/java/io/github/uptalent/auth/model/request/SponsorRegister.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class SponsorRegister extends AuthRegister{
