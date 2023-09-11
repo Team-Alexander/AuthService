17a007c8e21be8ad3236fbfbc774ece4ae30abf5
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-09-11 18:21:53 +0300

    Add Builder annotation to Auth classes

--- src/main/java/io/github/uptalent/auth/model/response/AuthResponse.java
+++ src/main/java/io/github/uptalent/auth/model/response/AuthResponse.java
@@ -2,9 +2,11 @@ package io.github.uptalent.auth.model.response;
 
 import io.github.uptalent.starter.security.Role;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class AuthResponse {
     private Long id;
